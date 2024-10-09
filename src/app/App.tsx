d1834298bcff657db99a04faae5c90069deefca4
kevin <ahadjon1999bb@gmail.com>
2024-10-09 16:24:43 +0900

    feat: integration footer and basket source

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import { Box, Button, Container, Stack, Typography } from '@mui/material';
-import { RippleBadge } from './MaterialTheme/styled';
-import { orange } from '@mui/material/colors';
 import { Link, Route, Switch, useLocation } from 'react-router-dom';
-import { About } from './screens/About';
-import { Users } from './screens/Users';
-import { HomePage } from './screens/homePage';
-import { ProductsPage } from './screens/productsPage';
-import { OrdersPage } from './screens/ordersPage';
-import { UserPage } from './screens/userPage';
-import { HomeNavbar } from './components/headers/HomeNavber';
-import { OtherNavbar } from './components/headers/OtherNavbar';
-import { Footer } from './components/footer';
+import HomePage from './screens/homePage';
+import ProductsPage from './screens/productsPage';
+import OrdersPage from './screens/ordersPage';
+import UserPage from './screens/userPage';
+import HomeNavbar from './components/headers/HomeNavber';
+import OtherNavbar from './components/headers/OtherNavbar';
+import Footer from './components/footer';
 import '../css/app.css';
 import "../css/navbar.css";
+import '../css/footer.css';
 import { HelpPage } from './screens/helpPage';
 
 function App() {
