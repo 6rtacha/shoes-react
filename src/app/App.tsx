7064e5924b33686e0ebeec9ba20d736418b80165
kevin <ahadjon1999bb@gmail.com>
2024-10-09 14:52:48 +0900

    feat: develop header and footer part one

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import '../css/app.css';
 import { Box, Button, Container, Stack, Typography } from '@mui/material';
 import { RippleBadge } from './MaterialTheme/styled';
 import { orange } from '@mui/material/colors';
@@ -13,6 +12,9 @@ import { UserPage } from './screens/userPage';
 import { HomeNavbar } from './components/headers/HomeNavber';
 import { OtherNavbar } from './components/headers/OtherNavbar';
 import { Footer } from './components/footer';
+import '../css/app.css';
+import "../css/navbar.css";
+import { HelpPage } from './screens/helpPage';
 
 function App() {
   const location = useLocation();
@@ -31,6 +33,9 @@ function App() {
           </Route>
           <Route path="/member-page">
             <UserPage />
+          </Route>
+          <Route path="/help">
+            <HelpPage />
           </Route>
           <Route path="/">
             <HomePage />
