7a475ab76ef23fbeb3897eed4bda3036d8706c92
kevin <ahadjon1999bb@gmail.com>
2024-11-03 15:55:21 +0900

    feat: create config & env ariables

---
+++ src/lib/config.ts
@@ -0,0 +1,9 @@
+export const serverApi: string = `${process.env.REACT_APP_API_URL}`;
+
+export const Messages = {
+  erro1: "Something went wrong!",
+  erro2: "Please login first!",
+  erro3: "Please fulfill all the inputs!",
+  erro4: "Message is empty!",
+  error5: "Only images with jpeg, jpg, png format allowed!",
+};
