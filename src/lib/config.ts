f65605b106070b07d240bdab40d93c23549fcb48
kevin <ahadjon1999bb@gmail.com>
2024-11-11 14:01:20 +0900

    feat: publish authentication modal component

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,9 +1,9 @@
 export const serverApi: string = `${process.env.REACT_APP_API_URL}`;
 
 export const Messages = {
-  erro1: "Something went wrong!",
-  erro2: "Please login first!",
-  erro3: "Please fulfill all the inputs!",
-  erro4: "Message is empty!",
+  error1: "Something went wrong!",
+  error2: "Please login first!",
+  error3: "Please fulfill all the inputs!",
+  error4: "Message is empty!",
   error5: "Only images with jpeg, jpg, png format allowed!",
 };
